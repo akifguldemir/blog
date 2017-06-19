675ed12ba4eeec5fd6132ad39aebd884d528c5ff
Akif Güldemir <akifguldemir@gmail.com>
2017-06-19 18:15:49 +0300

    Kullanıcı Listesi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        setLocale(LC_TIME,"tr_TR");
+        Carbon::setLocale('tr');
         config()->set("ayarlar",\App\Ayar::lists("value","name")->all());
         $this->app["form"]->component('bsText', 'form_components.text', ['name','label_name', 'value'=>null, 'attributes'=>[]]);
         $this->app["form"]->component('bsSubmit', 'form_components.submit', ['name', 'url'=>URL::previous()]);
